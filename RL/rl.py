ab18aed1c9e5e235fa53f55554a7f78d32b132bd
JaMussCraft <jamesnaruto04@gmail.com>
2025-04-05 09:30:42 -0400

    Added QTable-EG

--- RL/rl.py
+++ RL/rl.py
@@ -1,13 +1,19 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'environments')))
+
 import flappy_bird_gymnasium
 import gymnasium
-from environments.flappy_env import MultiplayerFlappyEnv
+from flappy_env import MultiplayerFlappyEnv
 # from stable_baselines3 import PPO
 
 # Create environment
 env = gymnasium.make("FlappyBird-v0", render_mode="human", use_lidar=False)
 
-# env = MultiplayerFlappyEnv(pipe_gap=130)
+# env = MultiplayerFlappyEnv(pipe_gap=130, render_mode="human")
 # env.current_player = "ai_agent" # Set active player for training
+# env.player_id = "ai_agent"
+# env.add_player("ai_agent")
 
 # Create and train a PPO agent
 # model = PPO("MlpPolicy", env, verbose=1)
@@ -23,7 +29,10 @@ while not done:
 
     # action, _ = model.predict(obs)
 
-    obs, reward, done, _, _ = env.step(action)
-    print(f"Action {action} gave {reward} reward...")
+    obs, reward, done, truncated, info = env.step(action)
+    # print(f"Action {action} gave {reward} reward...")
+    print(obs, reward, done, truncated, info)
+
+    # done = False
 
 env.close()
